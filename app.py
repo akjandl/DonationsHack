d7a188c89cf56cf28fed44e7aa9e687153f985ed
Mary Ellen Weber <mmweber2@gmail.com>
2017-09-24 01:27:55 -0500

    Change match_loc_data to return a list.

--- app.py
+++ app.py
@@ -5,56 +5,48 @@ from flask import Flask, render_template, g
 app = Flask(__name__)
 categories = set()
 
-# TODO: Get rid of this once merged
+# TODO: Dummy data; get rid of this once merged
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'test.db'),
     SECRET_KEY='dev',
     CATEGORIES=['shoes', 'bicycles'],
     LOCATIONS=[('Me', 'My phone', 'Always', 'My URL', 'My address'),
-                ('Poirot', 'Unlisted', 'Monday-Thursday', 'None', '221B Baker St')
-                ]
+               ('Poirot', 'Unlisted', 'Monday-Thursday', 'None', '221B Baker St')
+              ]
 ))
 
 # TODO: Mock: Remove later
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = sqlite3.connect(app.config['DATABASE'])
         g.sqlite_db.row_factory = sqlite3.Row
     return g.sqlite_db
 
-# TODO: Mock: Replace call later
+# TODO: Mock: Replace call with DB connection
 def populate_categories():
-    # Get categories only if they haven't been fetched yet
     global categories
+    # Get categories only if they haven't been fetched yet
     if not categories:
         #categories = g.sqlite_db.get_categories()
-        print("Categories are ")
-        print(app.config['CATEGORIES'])
         categories = app.config['CATEGORIES'] 
         return categories
 
-# TODO: Mock: Replace call later
+# TODO: Mock: Replace with call to DB
 def match_loc_data(category):
-    print("Returning ")
-    print(app.config['LOCATIONS'][0])
-    return app.config['LOCATIONS'][0]
+    return app.config['LOCATIONS']
 
 @app.route('/')
 def index():
     # TODO: Mock: Replace call later
     get_db()
+    global categories
     categories = populate_categories()
     return render_template('index.html', categories=categories)
 
 @app.route('/<category>')
 def retrieve_page(category):
-    global categories
     populate_categories()
-    #if category not in categories: TODO
-    if False:
+    if category not in categories:
         # Not a known category
         # TODO: What to render in this case?
         return render_template('index.html', categories=categories)
