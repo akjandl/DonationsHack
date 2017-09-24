5a796b0b15016a56b0edb86fe096afb5b606dfdb
Andy Jandl <akjandl@gmail.com>
2017-09-24 09:25:07 -0500

    database updated
    fixed issues with no unique records in Matches table

--- build_db.py
+++ build_db.py
@@ -34,20 +34,31 @@ def get_location_data(file):
     return locations
 
 
+def populate_matches(cursor, file):
+
+    with open(file, 'r') as data:
+        for l in data.readlines():
+            row = l.split('\t')
+            name = row[0].strip()
+            dt = row[1].strip()
+            val = row[2].strip()
+            if dt != 'category':
+                continue
+            # print(name, val)
+            loc_id = get_loc_id(cursor, name)[0]
+            cat_id = get_cat_id(cursor, val)[0]
+            add_match_rec_to_db(cursor, cat_id, loc_id)
+
+
+
 
 def main():
     con = sqlite3.connect('donations_hack.db')
     cursor = con.cursor()
     file = 'DonationData.tsv'
-    loc_data = get_location_data(file)
-    for key, data in loc_data.items():
-        name = key
-        phone = data.get('phone')
-        hours = data.get('hours')
-        website = data.get('website')
-        address = data.get('address')
-        add_loc_to_db(cursor, name, phone, hours, website, address)
+    populate_matches(cursor, file)
     con.commit()
+
 
 
 if __name__ == '__main__':
